925ea02c72e0fda540ced5636880ca9c9ec76fc0
ryuji <ryu.seven.h7420@gmail.com>
2020-09-07 07:59:41 +0900

    typesの追加とpostSliceの実装(extraReducer以外)

---
+++ src/features/post/postSlice.ts
@@ -0,0 +1,139 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+import axios from 'axios';
+import { PROPS_NEWPOST, PROPS_LIKED, PROPS_COMMENT } from '../types';
+import { create } from 'domain';
+
+const apiUrlPost = `${process.env.REACT_APP_DEV_API_URL}api/post/`;
+const apiUrlComment = `${process.env.REACT_APP_DEV_API_URL}api/comment/`;
+
+export const fetchAsyncGetPosts = createAsyncThunk('post/get', async () => {
+  const res = await axios.get(apiUrlPost, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+export const fetchAsyncNewPost = createAsyncThunk(
+  'post/post',
+  async (newPost: PROPS_NEWPOST) => {
+    const uploadData = new FormData();
+    uploadData.append('title', newPost.title);
+    newPost.img && uploadData.append('img', newPost.img, newPost.img.name);
+    const res = await axios.post(apiUrlPost, uploadData, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncPatchLiked = createAsyncThunk(
+  'post/patch',
+  async (liked: PROPS_LIKED) => {
+    const currentLiked = liked.current;
+    const uploadData = new FormData();
+
+    let isOverlapped = false;
+    currentLiked.forEach((current) => {
+      if (current === liked.new) {
+        isOverlapped = true;
+      } else {
+        uploadData.append('liked', String(current));
+      }
+    });
+
+    if (!isOverlapped) {
+      uploadData.append('liked', String(liked.new));
+    } else if (currentLiked.length === 1) {
+      uploadData.append('title', liked.title);
+      const res = await axios.put(`${apiUrlPost}${liked.id}/`, uploadData, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      });
+      return res.data;
+    }
+    const res = await axios.patch(`${apiUrlPost}${liked.id}/`, uploadData, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncGetComments = createAsyncThunk(
+  'comment/get',
+  async () => {
+    const res = await axios.get(apiUrlComment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncPostComment = createAsyncThunk(
+  'comment/post',
+  async (comment: PROPS_COMMENT) => {
+    const res = await axios.post(apiUrlComment, comment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const postSlice = createSlice({
+  name: 'post',
+  initialState: {
+    isLoadingPost: false,
+    openNewPost: false,
+    posts: [
+      {
+        id: 0,
+        title: '',
+        userPost: 0,
+        created_on: '',
+        img: '',
+        liked: [0],
+      },
+    ],
+    comments: [
+      {
+        id: 0,
+        text: '',
+        usetComment: 0,
+        post: 0,
+      },
+    ],
+  },
+  reducers: {
+    fetchPostStart(state) {
+      state.isLoadingPost = true;
+    },
+    fetchPostEnd(state) {
+      state.isLoadingPost = false;
+    },
+    setOpneNewPost(state) {
+      state.openNewPost = true;
+    },
+    resetOpenNewPost(state) {
+      state.openNewPost = false;
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const {} = postSlice.actions;
+
+export default postSlice.reducer;
