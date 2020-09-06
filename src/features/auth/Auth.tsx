9da9edda28d6f7552a14dce91fc5d355252c1b88
ryuji <ryu.seven.h7420@gmail.com>
2020-09-06 14:28:15 +0900

    新規登録用のモーダル実装

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -23,6 +23,7 @@ import {
   fetchAsyncGetProfs,
   fetchAsyncCreateProf,
 } from './authSlice';
+import { executionAsyncId } from 'async_hooks';
 
 const customStyles = {
   overlay: {
@@ -47,7 +48,115 @@ const Auth: React.FC = () => {
   const isLoadingAuth = useSelector(selectIsLoadingAuth);
   const dispatch: AppDispatch = useDispatch();
 
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <Modal
+        isOpen={openSignUp}
+        onRequestClose={async () => {
+          await dispatch(resetOpenSignUp());
+        }}
+        style={customStyles}
+      >
+        <Formik
+          initialErrors={{ email: 'required' }}
+          initialValues={{ email: '', password: '' }}
+          onSubmit={async (values) => {
+            await dispatch(fetchCredStart());
+            const resultReg = await dispatch(fetchAsyncRegister(values));
+
+            if (fetchAsyncRegister.fulfilled.match(resultReg)) {
+              await dispatch(fetchAsyncLogin(values));
+              await dispatch(fetchAsyncCreateProf({ nickName: 'anonymous' }));
+
+              await dispatch(fetchAsyncGetProfs());
+              // await dispatch(fetchAsyncGetPosts());
+              // await dispatch(fetchAsyncGetComments());
+              await dispatch(fetchAsyncGetMyProf());
+            }
+            await dispatch(fetchCredEnd());
+            await dispatch(resetOpenSignUp());
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email('email format is wrong')
+              .required('email is must'),
+            password: Yup.string().required('password is must').min(4),
+          })}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched,
+            isValid,
+          }) => (
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.auth_signUp}>
+                  <h1 className={styles.auth_title}>SNS clone</h1>
+                  <br />
+                  <div className={styles.auth_progress}>
+                    {isLoadingAuth && <CircularProgress />}
+                  </div>
+                  <br />
+
+                  <TextField
+                    placeholder="email"
+                    type="input"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <br />
+                  {touched.email && errors.email ? (
+                    <div className={styles.auth_error}>{errors.email}</div>
+                  ) : null}
+
+                  <TextField
+                    placeholder="password"
+                    type="input"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  <br />
+                  {touched.password && errors.password ? (
+                    <div className={styles.auth_error}>{errors.password}</div>
+                  ) : null}
+                  <br />
+                  <br />
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disabled={!isValid}
+                    type="submit"
+                  >
+                    Register
+                  </Button>
+                  <br />
+                  <br />
+                  <span
+                    className={styles.auth_text}
+                    onClick={async () => {
+                      await dispatch(setOpenSignIn());
+                      await dispatch(resetOpenSignUp());
+                    }}
+                  >
+                    You already have a account ?
+                  </span>
+                </div>
+              </form>
+            </div>
+          )}
+        </Formik>
+      </Modal>
+    </React.Fragment>
+  );
 };
 
 export default Auth;
