0c22c7159c7a11d37c5616846184769bdcef4294
ryuji <ryu.seven.h7420@gmail.com>
2020-09-09 07:48:11 +0900

    アバター部分の実装、投稿一覧の表示形式の実装(Postコンポーネントは未実装)

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -108,6 +108,7 @@ const Core: React.FC = () => {
               <MdAddAPhoto />
             </button>
             <div className={styles.core_logout}>
+              {(isLoadingPost || isLoadingAuth) && <CircularProgress />}
               <Button
                 onClick={() => {
                   localStorage.removeItem('localJWT');
@@ -119,12 +120,69 @@ const Core: React.FC = () => {
               >
                 Logout
               </Button>
+              <button
+                className={styles.core_btnModal}
+                onClick={() => {
+                  dispatch(setOpenProfile());
+                  dispatch(resetOpenNewPost());
+                }}
+              >
+                <StyledBadge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  variant="dot"
+                >
+                  <Avatar alt="who?" src={profile.img} />
+                  {''}
+                </StyledBadge>
+              </button>
             </div>
           </React.Fragment>
         ) : (
-          <div></div>
+          <div>
+            <Button
+              onClick={() => {
+                dispatch(setOpenSignIn());
+                dispatch(resetOpenSignUp());
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => {
+                dispatch(setOpenSignUp());
+                dispatch(resetOpenSignIn());
+              }}
+            >
+              SignUp
+            </Button>
+          </div>
         )}
       </div>
+      {profile?.nickName && (
+        <React.Fragment>
+          <Grid container spacing={4}>
+            {posts
+              .slice(0)
+              .reverse()
+              .map((post) => (
+                <Grid key={post.id} item xs={12} md={4}>
+                  <Post
+                    postId={post.id}
+                    title={post.title}
+                    loginId={profile.userProfile}
+                    userPost={post.userPost}
+                    imageUrl={post.img}
+                    liked={post.liked}
+                  />
+                </Grid>
+              ))}
+          </Grid>
+        </React.Fragment>
+      )}
     </div>
   );
 };
